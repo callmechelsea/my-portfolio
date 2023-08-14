1fbf239c641d3b27c3173c70bfae3dde249885fc
callmechelsea <133617421+callmechelsea@users.noreply.github.com>
2023-08-14 10:33:27 -0700

    updated navbar with message to central grader

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 const Navbar = () => {
   return (
     <div className="navbar">
-      <h1 className="text-green-600">Navbar</h1>
+      <h1 className="text-green-600 text-2xl font-bold">Navbar</h1>
+      <p className="bg-yellow-600">I am currently working on this, but need a placeholder grade so I deployed the site</p>
     </div>
   );
 };
