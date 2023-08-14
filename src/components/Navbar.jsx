83c416091653b872d8ebc06cc75cac2434ab7672
callmechelsea <133617421+callmechelsea@users.noreply.github.com>
2023-08-14 10:30:12 -0700

    might have missed changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <h1 className="text-green-600">Navbar</h1>
-        </div>
-    );
-    };
+  return (
+    <div className="navbar">
+      <h1 className="text-green-600">Navbar</h1>
+    </div>
+  );
+};
 
-    export default Navbar;
+export default Navbar;
