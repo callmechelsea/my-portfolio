25d57b587e0cc7dd7dd8baad181e6a846431d50e
Chelsea Pratte <cheese@Cheeses-iMac.local>
2024-01-11 17:53:42 -0800

    changed name back to my portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <h1 className="text-green-600 text-2xl font-bold">Navbar</h1>
-      <p className="bg-yellow-600">I am currently working on this, but need a placeholder grade so I deployed the site</p>
     </div>
   );
 };
